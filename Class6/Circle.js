917b5e0ff04f9fed7011bb018e29bf76e2097f3b
BomScoob <sorawitkraukham@gmail.com>
2023-09-21 10:59:36 +0700

    get raius method

--- Class6/Circle.js
+++ Class6/Circle.js
@@ -8,11 +8,17 @@ class Circle {
   area() {
     return Math.PI * Math.pow(this.radius, 2);
   }
+  getRadius() {
+    return this.radius;
+  }
 }
 
 const c1 = new Circle(1);
 const c2 = new Circle(2);
 const c3 = new Circle(4);
-console.log('object c1 : ', c1 ," and Area = "+ c1.area())
-console.log('object c2 : ', c2 ," and Area = "+ c2.area())
-console.log('object c3 : ', c3 ," and Area = "+ c3.area())
+console.log("object c1 : ", c1, " and Area = " + c1.area());
+console.log("object c2 : ", c2, " and Area = " + c2.area());
+console.log("object c3 : ", c3, " and Area = " + c3.area());
+console.log(c1.getRadius())
+console.log(c2.getRadius())
+console.log(c3.getRadius())
