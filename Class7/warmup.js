fd32a2b7fd2c9f8099028007f400492247642331
BomScoob <sorawitkraukham@gmail.com>
2023-09-28 09:28:45 +0700

    warmup function2

--- Class7/warmup.js
+++ Class7/warmup.js
@@ -1,6 +1,7 @@
 const arr = [1, 3, 6, 2, 10, 9];
 
 function findArraySummary(array) {
+  if (arr === null || arr === undefined) return;
   let outputObj = {
     min: Number.MAX_VALUE,
     max: Number.MIN_VALUE,
@@ -12,7 +13,8 @@ function findArraySummary(array) {
   for (let i of array) {
     if (outputObj.min > i) {
       outputObj.min = i;
-    } else if (outputObj.max < i) {
+    }
+    if (outputObj.max < i) {
       outputObj.max = i;
     }
     outputObj.sum += i;
@@ -24,4 +26,19 @@ function findArraySummary(array) {
   return outputObj;
 }
 
+function findArraySummary2(array) {
+  let min = array[0],
+    max = array[0],
+    sum = 0,
+    avg = 0;
+  for (const data of array) {
+    if (min > data) min = data;
+    if (max < data) max = data;
+    sum += data;
+  }
+  avg = sum/array.length
+  return {min: min, max: max, sum: sum, avg: avg}
+}
+
 console.log(findArraySummary(arr));
+console.log(findArraySummary2(arr));
