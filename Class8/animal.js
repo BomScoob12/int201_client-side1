75e9f7ce7b5cff30a2f12f2b2c9dabd34987809d
BomScoob <sorawitkraukham@gmail.com>
2023-10-05 11:41:39 +0700

    sort method array

--- Class8/animal.js
+++ Class8/animal.js
@@ -1,5 +1,4 @@
-
-const animals = ['ant', 'dogs', 'cats', 'bird']
+const animals = ['ant', 'dogs', 'cats', 'Bird']
 const message = 'Practices make perfect'
 //string includes - includes substring (case sensitive)
 console.log(message.includes('ice')) //true
@@ -9,6 +8,11 @@ console.log(animals.includes('ats')) //false
 console.log(animals.includes('cats')) //true
 
 console.log(animals)
-console.log(animals.reverse()) 
+console.log(animals.reverse())
 //array change
+console.log(animals)
+console.log('---------------------')
+//sort A-Z to a-z
+//[Bird, ant, ....]
+console.log(animals.sort())
 console.log(animals)
