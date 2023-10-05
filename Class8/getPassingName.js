5f690bad88ec2db07abb9e36d49059f1de154be9
BomScoob <sorawitkraukham@gmail.com>
2023-10-05 09:20:30 +0700

    array filter and array map method

--- Class8/getPassingName.js
+++ Class8/getPassingName.js
@@ -2,16 +2,16 @@ function getPassingNames(students) {
   const filterStudents = students
     .filter((student) => student.score >= 70)
     .map((student) => student.name.toUpperCase())
-  // Example usage:
-  const studentScores = [
-    { name: 'Alice', score: 85 },
-    { name: 'Bob', score: 92 },
-    { name: 'Charlie', score: 68 },
-    { name: 'David', score: 55 },
-    { name: 'Eve', score: 78 },
-  ]
-  const passingNames = getPassingNames(studentScores)
-  console.log(passingNames)
   // Output should be: ["ALICE", "BOB", "EVE"]
-  return studentNames
+  return filterStudents
 }
+
+const studentScores = [
+  { name: 'Alice', score: 85 },
+  { name: 'Bob', score: 92 },
+  { name: 'Charlie', score: 68 },
+  { name: 'David', score: 55 },
+  { name: 'Eve', score: 78 },
+]
+const passingNames = getPassingNames(studentScores)
+console.log(passingNames)
