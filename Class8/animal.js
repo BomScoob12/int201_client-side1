54f7fd0e93dc6c748ebd6db9758e3b0598e0cf90
BomScoob <sorawitkraukham@gmail.com>
2023-10-05 11:40:07 +0700

    include method array

---
+++ Class8/animal.js
@@ -0,0 +1,14 @@
+
+const animals = ['ant', 'dogs', 'cats', 'bird']
+const message = 'Practices make perfect'
+//string includes - includes substring (case sensitive)
+console.log(message.includes('ice')) //true
+console.log(message.includes('make')) //true
+//array includes - includes string in array elements (case sensitive)
+console.log(animals.includes('ats')) //false
+console.log(animals.includes('cats')) //true
+
+console.log(animals)
+console.log(animals.reverse()) 
+//array change
+console.log(animals)
--- Class8/fruit.js
+++ Class8/fruit.js
@@ -26,3 +26,18 @@ const totalSell = sellProduct.reduce(
   0
 )
 console.log(totalSell)
+
+
+//splice with remove
+//return an array containing the deleted elements.
+const removeElements = fruits.splice(1, 2)
+console.log(removeElements) //[ 'mango', 'orange' ]
+console.log(fruits) //[ 'apple', 'pineapple', 'banana', 'grape' ]
+//splice with add
+const deletedElements = fruits.splice(2, 0, 'mango', 'rambutant')
+console.log(deletedElements) //[]
+console.log(fruits) //[ 'apple', 'pineapple', 'mango', 'rambutant', 'banana', 'grape' ]
+//splice with replace
+const deletedFruit = fruits.splice(2, 1, 'mangosteen')
+console.log(deletedFruit) //[ 'mango' ]
+console.log(fruits)
